6e71d441214489784cb1d04cd5a2202fa29ab6bf
btaleka2 <btaleka2@illinois.edu>
2024-11-28 17:21:03 -0600

    Compression and data transfer fully working

--- GPUtransfer-compression.cu
+++ GPUtransfer-compression.cu
@@ -14,6 +14,19 @@
         }                                                                       \
     } while (0)
 
+void memoryCheck(int GPUnum) {
+    size_t free_memory, total_memory;
+    cudaError_t error = cudaMemGetInfo(&free_memory, &total_memory);
+    
+    if (error != cudaSuccess) {
+        std::cerr << "cudaMemGetInfo failed: " << cudaGetErrorString(error) << std::endl;
+    }
+    
+    std::cout << "For GPU " << GPUnum << " Free GPU memory: " << free_memory / (1024 * 1024) << " MB" << std::endl;
+    std::cout << "For GPU " << GPUnum << " Total GPU memory: " << total_memory / (1024 * 1024) << " MB" << std::endl;
+    std::cout << "For GPU " << GPUnum << " Used GPU memory: " << (total_memory - free_memory) / (1024 * 1024) << " MB" << std::endl;
+}
+
 void loadCheckpointFromDisk(const std::string& filename, std::vector<uint8_t>& data) {
     std::ifstream file(filename, std::ios::binary);
     if (!file) {
@@ -33,7 +46,7 @@ void loadCheckpointFromDisk(const std::string& filename, std::vector<uint8_t>& d
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Loaded checkpoint of size " << data.size() << " bytes." << std::endl;
+    std::cout << "Loaded checkpoint of size " << data.size() << " bytes in CPU memory." << std::endl;
 }
 
 int main() {
@@ -41,6 +54,8 @@ int main() {
     const std::string checkpointFile = "/work/hdd/bdof/nkanamarla/models/LLAMA3checkpointbinformat/LLAMA3checkpoint.bin";
     std::vector<uint8_t> weights;
     loadCheckpointFromDisk(checkpointFile, weights);
+
+    // Split up checkpoint code for next part
     auto middle = weights.begin() + weights.size() / 2;
     std::vector<uint8_t> weightsFirstHalf(weights.begin(), middle);
     std::vector<uint8_t> weightsSecondHalf(middle, weights.end());
@@ -66,6 +81,7 @@ int main() {
         return EXIT_FAILURE;
     }
     std::cout << "Setup peer access between source and destination GPU." << std::endl;
+    memoryCheck(srcDevice);
 
     // Set up CUDA stream
     CHECK_CUDA(cudaSetDevice(srcDevice));
@@ -82,7 +98,8 @@ int main() {
     size_t dataSize = weightsFirstHalf.size();
     CHECK_CUDA(cudaMalloc(&d_srcWeights, dataSize));
     CHECK_CUDA(cudaMemcpy(d_srcWeights, weightsFirstHalf.data(), dataSize, cudaMemcpyHostToDevice));
-    std::cout << "Weights transferred to source GPU " << srcDevice << " of size " << dataSize << " bytes." << std::endl;
+    std::cout << "Shard weights transferred to source GPU " << srcDevice << " of size " << dataSize << " bytes." << std::endl;
+    memoryCheck(srcDevice);
 
     {
     // Start timing
@@ -91,7 +108,7 @@ int main() {
     // Set up compression manager
     nvcompBatchedCascadedOpts_t cascade_options;
     cascade_options.type =  nvcomp::TypeOf<uint8_t>();
-    cascade_options.num_RLEs = 2;
+    cascade_options.num_RLEs = 1;
     cascade_options.num_deltas = 1;
     cascade_options.use_bp = 1;
     const size_t chunk_size = 1 << 22; // 4 MB chunks
@@ -104,6 +121,7 @@ int main() {
     // Compress data on source GPU
     nvcomp::CompressionConfig comp_config = cascade_manager.configure_compression(dataSize);
     uint8_t* d_compressedData;
+    comp_config.max_compressed_buffer_size = dataSize * 0.1; // nvCOMP is too conservative with the predicted compression size 
     std::cout << "Compression max possible size in bytes " << comp_config.max_compressed_buffer_size << std::endl;
     CHECK_CUDA(cudaMallocAsync(&d_compressedData, comp_config.max_compressed_buffer_size, stream));
     CHECK_CUDA(cudaStreamSynchronize(stream));
@@ -119,11 +137,13 @@ int main() {
     }
     CHECK_CUDA(cudaStreamSynchronize(stream));
     size_t compressed_size = cascade_manager.get_compressed_output_size(d_compressedData);
+    memoryCheck(srcDevice);
 
     // Allocate memory on destination GPU
     CHECK_CUDA(cudaSetDevice(dstDevice));
     uint8_t* d_dstCompressedData;
     CHECK_CUDA(cudaMalloc(&d_dstCompressedData, compressed_size));
+    memoryCheck(dstDevice);
 
     // Transfer compressed data between GPUs
     CHECK_CUDA(cudaSetDevice(srcDevice));
@@ -135,7 +155,7 @@ int main() {
 
     float milliseconds = 0;
     CHECK_CUDA(cudaEventElapsedTime(&milliseconds, start, stop));
-    std::cout << "Compressed data on source GPU of size " << compressed_size << " bytes for a compression ratio of " << dataSize/compressed_size << " X." << std::endl;
+    std::cout << "Compressed data on GPU of size " << compressed_size << " bytes for a compression ratio of " << dataSize/compressed_size << " X." << std::endl;
     std::cout << "Data Compression and GPU-to-GPU data transfer took " << milliseconds << " ms." << std::endl;
     CHECK_CUDA(cudaFree(d_compressedData));
     CHECK_CUDA(cudaSetDevice(dstDevice));
